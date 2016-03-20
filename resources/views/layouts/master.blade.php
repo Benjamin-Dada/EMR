0ccd5e6f96049b16a09597e90677ee6a6e6421dc
Benjamin Dada <Benjamin Dada>
2016-03-20 19:33:21 +0100

    no change made

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -50,19 +50,31 @@
                     <li><a href="{{ url('/home') }}">Home</a></li>
                 </ul>
  -->
+        @if (!Auth::check())
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
 
                     <!-- Authentication Links -->
                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <!-- <li><a href="{{ url('/register') }}">Register</a></li> -->
                 </ul>
+        @else       
+                <ul class="nav navbar-nav navbar-right">
 
+                <li class="dropdown">
+                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
 
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                      </ul>
+                </li>
+                </ul>
+        @endif
             </div>
         </div>
     </nav>
-    <!-- @include('layouts.partials.sidebar') -->
 
     @yield('content')
 
