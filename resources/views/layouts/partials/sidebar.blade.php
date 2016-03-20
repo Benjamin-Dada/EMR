fc5c5c7f3414a153ab0145cda021b5623ba59dd9
Benjamin Dada <Benjamin Dada>
2016-03-20 19:36:11 +0100

    Link img tag to user gravatar function. Add link to display user name. Add condition to only load sidebar if user is logged in

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,16 +1,17 @@
+@if(Auth::check())
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
         <li style="margin-left:20px;">
-            <img src="{{  asset('images/passport.png') }}" height="50" width="50" style="border-radius:25px;" />
+            <img src="{{ Auth::user()->getAvatarUrl() }}" height="50" width="50" style="border-radius:25px;" />
         </li>
-        <li><a href="#"> @ </a></li>
+        <li><a href="/"> @ {{Auth::user()->name}} </a></li>
         <li class="active"><a href="#">Patients<span class="sr-only">(current)</span></a></li>
         <li><a href="{{ route('patients.index') }}">Patient List</a></li>
-        <li><a href="#">Vitals</a></li>
     </ul>
     <ul class="nav nav-sidebar">
-        <li><a href="">Account</a></li>
-        <li><a href="">Help</a></li>
+        <li><a href="#">Stats</a></li>
+        <li><a href="#">Help</a></li>
         <li><a href="{{url('/logout')}}">Sign Out</a></li>
     </ul>
 </div>
+@endif
