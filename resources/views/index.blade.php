de605ad3a149b7bacbe62feae8b5fe33d58f475d
Benjamin Dada <Benjamin Dada>
2016-03-21 08:42:46 +0100

    improve the landing page view -  jumbotron, image, text, login button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,28 @@
 @section('content')
 
 @if(!Auth::check())
-<div class="container">
-    <div class="row">
-     <h1>Welcome to my EMR</h1>
-     <p>Please log in</p>
+    <div class="jumbotron">
+    <div class="container">
+    <h1>Welcome to EMR</h1>
+    <p><img src="http://res.cloudinary.com/dgpjdyg8p/image/upload/v1458521045/With_Doctors_eh80vm.jpg" alt="Hospital_staff_image" style="width: 100%;border-radius: 5px;">
+    </p>
+        <p><strong>The aim of this system is to design develop a cloud-based EMR solution for a particular hospital.</strong></p>
+        <p><h4><strong>The Objective of this project is to: </strong></h4></p>
+        <ol>
+            <h4><strong><li>Analyze the existing Medical Record Information System currently in use by developed Countries so as to utilize better development and design techniques.</li></strong></h4>
+            <h4><strong><li>Identify the requirements for development of an Automated Medical Record Information System</li></strong></h4>
+            <h4><strong><li>Model and design a robust EMR using Unified Modelling Language Tools</li></strong></h4>
+            <h4><strong><li>Implement a reliable EMR based off my research</li></strong></h4>
+            <h4><strong><li>Evaluate the EMR system to be developed</li></strong></h4>
+        </ol>
+    <p><a class="btn btn-primary btn-lg" href="{{url('/login')}}" role="button">Login &raquo;</a></p>
     </div>
+</div>
+<div class="footer">
+  <div class="container">
+    <p style="padding-top:20px; text-align: center; font-family: 'Open Sans';"> &copy; <?php echo date("Y"); ?>
+ | Made with <i class="fa fa-heart"></i> by Benjamin Dada</p>
+  </div>
 </div>
 @endif
 
