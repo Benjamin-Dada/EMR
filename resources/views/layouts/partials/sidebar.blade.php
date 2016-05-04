92e05ea7f2dcc0905a9a459db4ba41c762a9b621
Benjamin Dada <Benjamin Dada>
2016-05-04 21:20:33 +0100

    reference the Img folder properly by converting the letter I to uppercase

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,41 +2,41 @@
 <div class="col-sm-3 col-md-2 sidebar">
     @if(Auth::user()->role === "0")
     <div class="pull-left" style="padding-bottom: 10px;">
-        <img src="{{asset('/images/img/user8-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
+        <img src="{{asset('/images/Img/user8-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
     </div>
     <div style="color: #fff; position: absolute; left: 65px; padding: 5px 5px 5px 20px;">{{Auth::user()->name}}<br><small>Admin</small></div>
     @endif
 
     @if(Auth::user()->role==="1")
     <div class="pull-left" style="padding-bottom: 10px;">    
-        <img src="{{asset('/images/img/user7-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
+        <img src="{{asset('/images/Img/user7-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
     </div>
     <div style="color: #fff; position: absolute; left: 65px; padding: 5px 5px 5px 20px; ">{{Auth::user()->name}}<br><small>Front Desk</small></div>
     @endif
 
     @if(Auth::user()->role==="2")
     <div class="pull-left" style="padding-bottom: 10px;"> 
-        <img src="{{asset('/images/img/user5-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
+        <img src="{{asset('/images/Img/user5-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
      </div>
     <div style="color: #fff; position: absolute; left: 65px; padding: 5px 5px 5px 20px; ">{{Auth::user()->name}}<br><small>Nurse</small></div>
     @endif
 
     @if(Auth::user()->role==="3" ) 
     <div class="pull-left" style="padding-bottom: 10px;"> 
-        <img src="{{asset('/images/img/user6-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
+        <img src="{{asset('/images/Img/user6-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
     </div>
     <div style="color: #fff; position: absolute; left: 65px; padding: 5px 5px 5px 20px; ">{{Auth::user()->name}}<br><small>Doctor</small></div>
     @endif
 
     @if(Auth::user()->role==="4" ) 
     <div class="pull-left" style="padding-bottom: 10px;"> 
-        <img src="{{asset('/images/img/user6-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
+        <img src="{{asset('/images/Img/user6-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
     </div>
     <div style="color: #fff; position: absolute; left: 65px; padding: 5px 5px 5px 20px; ">{{Auth::user()->name}}<br><small>Lab</small></div>
     @endif
     @if(Auth::user()->role==="5" ) 
     <div class="pull-left" style="padding-bottom: 10px;"> 
-        <img src="{{asset('/images/img/user6-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
+        <img src="{{asset('/images/Img/user6-128x128.jpg')}}" class="img-circle" alt="User Image" height="50" weight="50" />
     </div>
     <div style="color: #fff; position: absolute; left: 65px; padding: 5px 5px 5px 20px; ">{{Auth::user()->name}}<br><small>Pharmacy</small></div>
     @endif
